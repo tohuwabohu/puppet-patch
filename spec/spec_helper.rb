d8b969c2c14e11c423074d3cc1e11b57eb18039d
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-03-22 19:19:45 +1100

    Use puppetlabs_spec_helper instead of plain rspec-puppet
    The puppetlabs_spec_helper will download the specified dependencies
    before running tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,1 @@
-require 'rspec-puppet'
-
-RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
-  c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'manifests'))
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
