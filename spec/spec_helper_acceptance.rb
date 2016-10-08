a8ed6639a2af2feb5f1fcc9ccfe0ae5fbb1586bc
Martin Meinhold <Martin.Meinhold@gmx.de>
2016-10-08 21:00:26 +1100

    Remove redundant code

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -7,16 +7,12 @@ end
 
 RSpec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  ignore_list = %w(junit log spec tests vendor)
 
   c.formatter = :documentation
 
   c.before :suite do
+    puppet_module_install(:source => proj_root, :module_name => 'patch')
     hosts.each do |host|
-      # Install module
-      copy_module_to(host, :source => proj_root, :module_name => 'patch', :ignore_list => ignore_list)
-
-      # Install dependencies
       on host, puppet('module', 'install', 'puppetlabs-stdlib', '--version 4.12.0')
     end
   end
